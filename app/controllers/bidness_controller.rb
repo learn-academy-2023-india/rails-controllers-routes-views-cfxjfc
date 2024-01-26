2b8192fe9d7a0f844b029bc5e3932f9d7e457e04
jayfr1122 <jayfranks21@gmail.com>
2024-01-25 16:31:38 -0800

    working business site with xavier and chantel

---
+++ app/controllers/bidness_controller.rb
@@ -0,0 +1,10 @@
+class BidnessController < ApplicationController
+    def home
+    end
+    def about
+    end
+    def contact
+    end
+    def products
+    end
+end
---
+++ app/helpers/bidness_helper.rb
@@ -0,0 +1,2 @@
+module BidnessHelper
+end
